d1f4be610cf71ad8ee83052c6df553d39aa36201
Jack Stanek <stanekjack@gmail.com>
2023-02-23 23:48:27 -0600

    Update flake to build run app

--- flake.nix
+++ flake.nix
@@ -1,14 +1,26 @@
 {
- description = "Script for automatically running backups";
+  description = "Script for automatically running backups";
 
- inputs = {
-           nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-           flake-utils.url = "github:numtide/flake-utils";
-           };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
- outputs = { self, nixpkgs, flake-utils }:
-   flake-utils.lib.simpleFlake {
-     inherit self nixpkgs;
-     name = "automatic-restic";
-   };
- }
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      rec {
+        defaultPackage =
+          with import nixpkgs { system = "${system}"; };
+          stdenv.mkDerivation {
+            name = "automatic-restic";
+            src = self;
+            installPhase = "mkdir -p $out/bin; install -t $out/bin run_backup.sh";
+          };
+        apps.automatic-restic = flake-utils.lib.mkApp {
+          drv = defaultPackage;
+          exePath = "/bin/run_backup.sh";
+        };
+        defaultApp = apps.automatic-restic;
+      }
+    );
+}
